5c8484dee5f9ab7c35511fedcb30ed861b486cbd
Maksym Shekhovets <maxymshg@gmail.com>
2022-05-05 22:41:28 +0300

    Solana Airdrop quest solution

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { Connection, PublicKey, clusterApiUrl, Keypair, LAMPORTS_PER_SOL, Transaction } = require("@solana/web3.js");
+
+const newPair = new Keypair();
+const publicKey = new PublicKey(newPair.publicKey).toString();
+const secretKey = newPair.secretKey;
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`=> For wallet address ${publicKey}`);
+        console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = Keypair.fromSecretKey(secretKey);
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const driverFunction = async () => {
+    await getWalletBalance();
+    await airDropSol();
+    await getWalletBalance();
+};
+
+driverFunction();
